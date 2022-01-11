aed41da6f7c360f0c3ce8938d27f4d3d531227e9
AndroidUser4438 <michael@lachut.com>
2022-01-11 16:11:42 -0500

    ADJUSTED THE PARAMETERS
    Signed-off-by: AndroidUser4438 <michael@lachut.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/utils/autonomous/PipelineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/utils/autonomous/PipelineTest.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.main.utils.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.main.utils.autonomous.location.pipeline.PositionSystem;
 import org.firstinspires.ftc.teamcode.main.utils.autonomous.sensors.NavigationSensorCollection;
+import org.firstinspires.ftc.teamcode.main.utils.gamepads.GamepadManager;
 import org.firstinspires.ftc.teamcode.main.utils.interactions.items.StandardDistanceSensor;
 import org.firstinspires.ftc.teamcode.main.utils.interactions.items.StandardIMU;
 import org.firstinspires.ftc.teamcode.main.utils.resources.Resources;
@@ -31,6 +33,6 @@ public class PipelineTest extends LinearOpMode {
             telemetry.addData("    East", positionSystem.rawEastReading);
             telemetry.addData("    West", positionSystem.rawWestReading);
             telemetry.update();
-       }
+        }
     }
 }
