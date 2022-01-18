09e8405cb1bde79812dc714e382b3a12728a2de3
colinteahan <24cteahan@xbhs.net>
2022-01-18 10:21:43 -0500

    Update FullTeleOpScript.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/scripts/teleop/FullTeleOpScript.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/scripts/teleop/FullTeleOpScript.java
@@ -94,8 +94,8 @@ public class FullTeleOpScript extends TeleOpScript {
     }
 
     private void controlDrivetrain() {
-        int left = (int) Range.clip((gamepadManager.functionOneGamepad().right_stick_y + gamepadManager.functionOneGamepad().right_stick_x) * 75, -75, 75);
-        int right = (int) Range.clip((gamepadManager.functionOneGamepad().right_stick_y - gamepadManager.functionOneGamepad().right_stick_x) * 75, -75, 75);
+        int left = (int) Range.clip((gamepadManager.functionOneGamepad().left_stick_y + gamepadManager.functionOneGamepad().left_stick_x) * 75, -75, 75);
+        int right = (int) Range.clip((gamepadManager.functionOneGamepad().left_stick_y - gamepadManager.functionOneGamepad().left_stick_x) * 75, -75, 75);
         inputSpace.sendInputToTank(TankDrivetrainLocation.Action.SET_SPEED, -right, -left);
     }
 
