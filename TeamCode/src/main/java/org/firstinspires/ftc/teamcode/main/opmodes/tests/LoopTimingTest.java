f99dbad0544c0b3909dabc5b37a07c5502164e79
AndroidUser4438 <michael@lachut.com>
2022-02-01 15:43:32 -0500

    Made test for the time that a loop takes
    Signed-off-by: AndroidUser4438 <michael@lachut.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/opmodes/tests/LoopTimingTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.main.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.main.utils.scripting.AutonomousStubScript;
+import org.firstinspires.ftc.teamcode.main.utils.scripting.Script;
+import org.firstinspires.ftc.teamcode.main.utils.scripting.ScriptRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@Autonomous(name="Loop Timing Test", group="tests")
+public class LoopTimingTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        double avg = 0;
+        ElapsedTime timer = new ElapsedTime();
+
+        timer.reset();
+
+        while (opModeIsActive()) {
+            avg = avg + timer.time(TimeUnit.MILLISECONDS);
+            telemetry.addData("Average Loop Time (ms)", avg);
+            telemetry.addData("Current Loop Time (ms)", timer.time(TimeUnit.MILLISECONDS));
+            telemetry.update();
+
+            timer.reset();
+        }
+    }
+}
