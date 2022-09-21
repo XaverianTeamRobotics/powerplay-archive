0fe5a31c0dcb07a89d750862f642a3b34186927c
Thomas Ricci <git@thomasricci.dev>
2022-09-21 14:36:54 -0400

    test 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/hardware/HardwareGetter.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/hardware/HardwareGetter.kt
@@ -15,6 +15,7 @@ import org.firstinspires.ftc.teamcode.utils.hardware.requests.GamepadRequest
 import org.firstinspires.ftc.teamcode.utils.hardware.requests.MotorRequest
 import java.lang.IllegalArgumentException
 
+// more tests
 class HardwareGetter {
     companion object {
         @JvmStatic
