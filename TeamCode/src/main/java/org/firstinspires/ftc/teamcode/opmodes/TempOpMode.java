e10f391da4996a77fb5010d7bc41a89cda91360b
Thomas Ricci <git@thomasricci.dev>
2022-09-22 12:47:50 -0400

    quick opmode for robot in two weeks updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TempOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TempOpMode.java
@@ -46,6 +46,16 @@ public class TempOpMode extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
+
+            // ugh that explanation above makes NO sense so here it is in simple thomas ricci is not smart enough to
+            // understand it terms: I have NO idea what going on he- no just kidding.
+            //
+            // basically since we're working with ratios the denom is the highest motor value we possibly can have so
+            // nothing is out of alignment
+
+            // (for example, if you were driving at full speed forward but only half to the side, we need to
+            // calculate the side as a fraction of the forward speed or else the robot would assume both sides are
+            // going at full speed and That Is Bad.
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
@@ -63,4 +73,3 @@ public class TempOpMode extends LinearOpMode {
         }
     }
 }
-
