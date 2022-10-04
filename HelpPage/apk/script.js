77c05f924a880b786fc12a371749d2be22f1a0c9
Colin Teahan <73704575+24cteahan@users.noreply.github.com>
2022-10-04 14:44:38 -0400

    Update script.js

--- HelpPage/apk/script.js
+++ HelpPage/apk/script.js
@@ -1,5 +1,6 @@
 $.getJSON("https://api.github.com/repos/FIRST-Tech-Challenge/FtcRobotController/releases").done(function (json) {
     var downloadURL = json[0].assets[0].browser_download_url;
+    console.log(downloadURL)
     $("#data").attr({
         "href": downloadURL
     })
