dccfb3c62bb71bd5ed1aa61f4aec44e6a3c9f589
michaell4438 <michael@lachut.com>
2022-10-07 21:53:16 -0400

    Some quick changes to the SleeveColorDetection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internals/image/SleeveColorDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internals/image/SleeveColorDetection.java
@@ -7,10 +7,8 @@ import org.opencv.features2d.SimpleBlobDetector_Params;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-import java.lang.reflect.Array;
-
+import static org.firstinspires.ftc.teamcode.internals.image.ImageProcessingConstants.*;
 import static org.opencv.core.Core.*;
-import static org.opencv.features2d.Features2d.drawKeypoints;
 import static org.opencv.imgproc.Imgproc.*;
 
 public class SleeveColorDetection extends OpenCvPipeline {
@@ -21,10 +19,10 @@ public class SleeveColorDetection extends OpenCvPipeline {
 
     @Override
     public Mat processFrame(Mat input) {
-        Logging.logData("Input Height", input.height());
-        Logging.logData("Input Width", input.width());
+        //Logging.logData("Input Height", input.height());
+        //Logging.logData("Input Width", input.width());
 
-        double cropRatio = ImageProcessingConstants.CROP_RATIO;
+        double cropRatio = CROP_RATIO;
 
         // Crop part of the image to focus the center of the image
         Rect roi = new Rect(    new Point(input.width()*cropRatio, input.height()*cropRatio),
@@ -33,7 +31,7 @@ public class SleeveColorDetection extends OpenCvPipeline {
         Mat processedMat = input.submat(roi);
 
         // Blur both the image to reduce noise
-        Size blurSize = new Size(ImageProcessingConstants.GAUSSIAN_BLUR_SIZE, ImageProcessingConstants.GAUSSIAN_BLUR_SIZE);
+        Size blurSize = new Size(GAUSSIAN_BLUR_SIZE, GAUSSIAN_BLUR_SIZE);
         GaussianBlur(processedMat, processedMat, blurSize, 0);
 
         // Convert to grayscale
@@ -43,22 +41,22 @@ public class SleeveColorDetection extends OpenCvPipeline {
         cvtColor(processedMat, processedMat, Imgproc.COLOR_RGB2HSV);
 
         // Define the colors that we want to detect
-        Scalar redLowHSV = new Scalar(ImageProcessingConstants.RED_H_MIN, ImageProcessingConstants.RED_S_MIN, ImageProcessingConstants.RED_V_MIN);
-        Scalar redHighHSV = new Scalar(ImageProcessingConstants.RED_H_MAX, ImageProcessingConstants.RED_S_MAX, ImageProcessingConstants.RED_V_MAX);
+        Scalar redLowHSV = new Scalar(RED_H_MIN, RED_S_MIN, RED_V_MIN);
+        Scalar redHighHSV = new Scalar(RED_H_MAX, RED_S_MAX, RED_V_MAX);
 
-        Scalar blueLowHSV = new Scalar(ImageProcessingConstants.BLUE_H_MIN, ImageProcessingConstants.BLUE_S_MIN, ImageProcessingConstants.BLUE_V_MIN);
-        Scalar blueHighHSV = new Scalar(ImageProcessingConstants.BLUE_H_MAX, ImageProcessingConstants.BLUE_S_MAX, ImageProcessingConstants.BLUE_V_MAX);
+        Scalar blueLowHSV = new Scalar(BLUE_H_MIN, BLUE_S_MIN, BLUE_V_MIN);
+        Scalar blueHighHSV = new Scalar(BLUE_H_MAX, BLUE_S_MAX, BLUE_V_MAX);
 
-        Scalar greenLowHSV = new Scalar(ImageProcessingConstants.GREEN_H_MIN, ImageProcessingConstants.GREEN_S_MIN, ImageProcessingConstants.GREEN_V_MIN);
-        Scalar greenHighHSV = new Scalar(ImageProcessingConstants.GREEN_H_MAX, ImageProcessingConstants.GREEN_S_MAX, ImageProcessingConstants.GREEN_V_MAX);
+        Scalar greenLowHSV = new Scalar(GREEN_H_MIN, GREEN_S_MIN, GREEN_V_MIN);
+        Scalar greenHighHSV = new Scalar(GREEN_H_MAX, GREEN_S_MAX, GREEN_V_MAX);
 
-        autoCalibrateBackgroundFilter(grayScale);
+        MatOfKeyPoint grayscaleBlobs = autoCalibrateBackgroundFilter(grayScale);
 
         // Remove the background from the gray image and just get the cone to use as a mask and then convert back to hsv
         inRange(
             grayScale,
-            new Scalar(ImageProcessingConstants.GRAY_MIN),
-            new Scalar(ImageProcessingConstants.GRAY_MAX),
+            new Scalar(GRAY_MIN),
+            new Scalar(GRAY_MAX),
             grayScale
         );
         // Set the GRAY_MIN and GRAY_MAX values to 0 and 255, respectively, to disable the feature
@@ -93,12 +91,16 @@ public class SleeveColorDetection extends OpenCvPipeline {
 
         Logging.updateLog();
 
-        if (ImageProcessingConstants.RETURN_GRAYSCALE) {
+        if (RETURN_GRAYSCALE) {
             processedMat.release();
             return grayScale; // Useful for tuning the background filtering
         } else {
             grayScale.release();
             cvtColor(processedMat, processedMat, COLOR_HSV2RGB); // Convert back to RGB for display
+            for (KeyPoint blob : grayscaleBlobs.toList()) {
+                // Generate a circle given the points and size of the blob
+                circle(processedMat, new Point(blob.pt.x, blob.pt.y), (int) blob.size/2, new Scalar(0, 255, 0), 3);
+            }
             return processedMat; // Useful for previewing the final output
         }
     }
@@ -135,43 +137,74 @@ public class SleeveColorDetection extends OpenCvPipeline {
      * <br>
      * This method will automatically change the constants in the ImageProcessingConstants class. No value will be returned
      * @param input The grayscale image to process
+     * @return The blobs found in the image after attepmting background filtering
      */
-    public void autoCalibrateBackgroundFilter(Mat input) {
+    public MatOfKeyPoint autoCalibrateBackgroundFilter(Mat input) {
         int iterations = 0;
-        while (iterations <= ImageProcessingConstants.MAX_BACKGROUND_FILTER_ADJUSTMENT_ITERATIONS) {
+        while (true) {
             iterations++;
             Mat mat = input.clone();
             // Remove the background from the gray image
             inRange(
                 mat,
-                new Scalar(ImageProcessingConstants.GRAY_MIN),
-                new Scalar(ImageProcessingConstants.GRAY_MAX),
+                new Scalar(GRAY_MIN),
+                new Scalar(GRAY_MAX),
                 mat
             );
+
+            // Run blob detection on the binary mask
+            // The goal is not to make the mask just the cone, but to adjust the boundaries so that the area AROUND the cone is removed
+            // If we set the mask to be just the cone, there is the odd chance that opencv will detect something else as the cone
+            // This helps if we choose to use zoom later
+            // Again, proper calibration of GRAYSCALE_BLOB_MIN_AREA and GRAYSCALE_BLOB_MAX_AREA is required
             MatOfKeyPoint keypoints = new MatOfKeyPoint();
             SimpleBlobDetector_Params grayscaleBlobParameters = new SimpleBlobDetector_Params();
-            grayscaleBlobParameters.set_minArea((float) ImageProcessingConstants.GRAYSCALE_BLOB_MIN_AREA);
-            grayscaleBlobParameters.set_maxArea((float) ImageProcessingConstants.GRAYSCALE_BLOB_MAX_AREA);
+            grayscaleBlobParameters.set_minArea((float) GRAYSCALE_BLOB_MIN_AREA);
+            grayscaleBlobParameters.set_maxArea((float) GRAYSCALE_BLOB_MAX_AREA);
+
+            // Binary mask (our input) is either 0 or 255, so we need to set the threshold to 254 to get the blob detection to work for bright colors
+            grayscaleBlobParameters.set_minThreshold(254);
+            grayscaleBlobParameters.set_maxThreshold(255);
+
+            // We only care about the area and color, so we can tell opencv to ignore the rest of the parameters
+            grayscaleBlobParameters.set_filterByArea(true);
+            grayscaleBlobParameters.set_filterByCircularity(false);
+            grayscaleBlobParameters.set_filterByConvexity(false);
+            grayscaleBlobParameters.set_filterByInertia(false);
+            grayscaleBlobParameters.set_filterByColor(true);
+
             SimpleBlobDetector detector = SimpleBlobDetector.create();
             detector.detect(mat, keypoints);
 
+            // If there is only one blob, we have found the correct values
+            // For the GRAY_MIN and GRAY_MAX values, adjust them if there are too many or too few blobs
             KeyPoint[] keypointsArray = keypoints.toArray();
             if (keypointsArray != null) {
                 if (keypointsArray.length == 1) {
-                    return;
+                    return keypoints;
                 } else if (keypointsArray.length > 1) {
-                    ImageProcessingConstants.GRAY_MIN += 5;
-                    ImageProcessingConstants.GRAY_MAX -= 5;
+                    // Decide whether or not we should increase the GRAY_MIN or decrease the GRAY_MAX
+                    // If the GRAY_MIN is below 20, then we should do nothing.
+                    if (GRAY_MIN >= 25) {
+                        GRAY_MIN += 5;
+                        GRAY_MAX -= 5;
+                    }
                 } else {
-                    ImageProcessingConstants.GRAY_MIN -= 5;
-                    ImageProcessingConstants.GRAY_MAX += 5;
+                    // Do the opposite of the above, but GRAY_MAX shouldnt go above 235
+                    if (GRAY_MAX <= 230) {
+                        GRAY_MIN -= 5;
+                        GRAY_MAX += 5;
+                    }
                 }
-                Logging.logData("GRAY_MIN", ImageProcessingConstants.GRAY_MIN);
-                Logging.logData("GRAY_MAX", ImageProcessingConstants.GRAY_MAX);
-                Logging.updateLog();
             }
+            // Force GRAY_MIN and GRAY_MAX to be within the range of 0-255, and the difference between GRAY_MIN and GRAY_MAX to be at least 50
+            GRAY_MIN = Math.max(0, Math.min(205, GRAY_MIN));
+            GRAY_MAX = Math.max(50, Math.min(255, GRAY_MAX));
+            GRAY_MAX = Math.max(GRAY_MIN + 50, GRAY_MAX);
+            GRAY_MIN = Math.min(GRAY_MAX - 50, GRAY_MIN);
+
             mat.release();
-            if (iterations >= ImageProcessingConstants.MAX_BACKGROUND_FILTER_ADJUSTMENT_ITERATIONS) return;
+            if (iterations >= MAX_BACKGROUND_FILTER_ADJUSTMENT_ITERATIONS) return keypoints;
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OpenCvSleeveDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OpenCvSleeveDetectionTest.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import android.annotation.SuppressLint;
 import com.acmerobotics.dashboard.FtcDashboard;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.internals.hardware.HardwareGetter;
 import org.firstinspires.ftc.teamcode.internals.hardware.Logging;
+import org.firstinspires.ftc.teamcode.internals.image.ImageProcessingConstants;
 import org.firstinspires.ftc.teamcode.internals.image.SleeveColorDetection;
 import org.firstinspires.ftc.teamcode.internals.registration.OperationMode;
 import org.firstinspires.ftc.teamcode.internals.registration.TeleOperation;
@@ -11,15 +12,17 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
+import java.util.Objects;
+
 import static org.firstinspires.ftc.teamcode.internals.hardware.HardwareGetter.getHardwareMap;
 
 public class OpenCvSleeveDetectionTest extends OperationMode implements TeleOperation {
 
     public SleeveColorDetection detector;
-
+    @SuppressLint("DiscouragedApi")
     @Override
     public void construct() {
-        WebcamName webcamName = getHardwareMap().get(WebcamName.class, "camera0");
+        WebcamName webcamName = Objects.requireNonNull(getHardwareMap()).get(WebcamName.class, "camera0");
         int cameraMonitorViewId = getHardwareMap().appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
 
@@ -31,17 +34,18 @@ public class OpenCvSleeveDetectionTest extends OperationMode implements TeleOper
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
             @Override
-            public void onOpened()
-            {
-                // Usually this is where you'll want to start streaming from the camera (see section 4)
-                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-            }
+            public void onOpened() { camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT); }
             @Override
             public void onError(int errorCode)
             {
                 /*
                  * This will be called if the camera could not be opened
                  */
+
+                Logging.logData("Camera error", errorCode);
+                Logging.updateLog();
+                sleep(2000);
+                stop();
             }
         });
     }
@@ -49,6 +53,10 @@ public class OpenCvSleeveDetectionTest extends OperationMode implements TeleOper
     @Override
     public void run() {
         Logging.logData("Sleeve Color ID", detector.getDetection());
+
+        // Code here helps determine the effectiveness of the brightness adjustment feature
+        Logging.logData("GRAY_MIN", ImageProcessingConstants.GRAY_MIN);
+        Logging.logData("GRAY_MAX", ImageProcessingConstants.GRAY_MAX);
         Logging.updateLog();
     }
 }
