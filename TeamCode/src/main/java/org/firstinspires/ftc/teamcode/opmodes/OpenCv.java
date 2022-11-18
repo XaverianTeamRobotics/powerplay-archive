0c073d29a07d7a18bf0cc7a5d8fa0950a377dbbf
colinteahan <24cteahan@xbhs.net>
2022-11-18 14:56:26 -0500

    merge prep

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OpenCv.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import org.firstinspires.ftc.teamcode.internals.hardware.Devices;
+import org.firstinspires.ftc.teamcode.internals.registration.OperationMode;
+import org.firstinspires.ftc.teamcode.internals.registration.TeleOperation;
+
+public class OpenCv extends OperationMode implements TeleOperation {
+    @Override
+    public void construct() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
