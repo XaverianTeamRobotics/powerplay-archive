973a8f7344078df3a95937c80ec38d23c045232b
Thomas Ricci <git@thomasricci.dev>
2022-12-09 08:35:48 -0500

    admissions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Kids.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import org.firstinspires.ftc.teamcode.features.MecanumDrivetrainFeature;
+import org.firstinspires.ftc.teamcode.internals.misc.DrivetrainMapMode;
+import org.firstinspires.ftc.teamcode.internals.registration.OperationMode;
+import org.firstinspires.ftc.teamcode.internals.registration.TeleOperation;
+
+public class Kids extends OperationMode implements TeleOperation {
+
+    @Override
+    public void construct() {
+        registerFeature(new MecanumDrivetrainFeature(DrivetrainMapMode.FR_BR_FL_BL, false, false, false));
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
