45b0056bfaf9f1f08e82d101f0abc3245f5974b2
Michael Lachut <michael@lachut.com>
2023-01-03 15:57:44 -0500

    Changed the SleeveDetectionFeature and added a test for it (SleeveDetectionTest)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SleeveDetectionTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import org.firstinspires.ftc.teamcode.features.SleeveDetectionFeature;
+import org.firstinspires.ftc.teamcode.internals.registration.AutonomousOperation;
+import org.firstinspires.ftc.teamcode.internals.registration.OperationMode;
+import org.firstinspires.ftc.teamcode.internals.telemetry.Logging;
+
+public class SleeveDetectionTest extends OperationMode implements AutonomousOperation {
+
+    SleeveDetectionFeature detector;
+
+    @Override
+    public Class<? extends OperationMode> getNext() {
+        return null;
+    }
+
+    @Override
+    public void construct() {
+        detector = new SleeveDetectionFeature();
+    }
+
+    @Override
+    public void run() {
+        Logging.logData("Detected Spot:", detector.getSpot());
+    }
+}
