32aa140eea8aa43b6f15958d578f1fca13d14fde
Michael Lachut <michael@lachut.com>
2023-01-26 09:03:56 -0500

    Four motor arm manual leveling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/FourMotorArm.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/FourMotorArm.kt
@@ -4,6 +4,7 @@ import org.firstinspires.ftc.teamcode.internals.features.Buildable
 import org.firstinspires.ftc.teamcode.internals.features.Feature
 import org.firstinspires.ftc.teamcode.internals.hardware.Devices
 import org.firstinspires.ftc.teamcode.internals.hardware.Devices.Companion.controller1
+import org.firstinspires.ftc.teamcode.internals.hardware.Devices.Companion.controller2
 
 class FourMotorArm: Feature(), Buildable {
 
@@ -11,10 +12,14 @@ class FourMotorArm: Feature(), Buildable {
 
     override fun loop() {
         // Motor config: 4 - TL, 5 - BL, 6 - TR, 7 - BR
-        val power0 = controller1.rightTrigger - (controller1.leftTrigger * 50)
-        Devices.motor4.speed = power0
-        Devices.motor5.speed = -power0
-        Devices.motor6.speed = power0
-        Devices.motor7.speed = -power0
+        val powerL = controller1.rightTrigger - (controller1.leftTrigger * 0.5)
+        val powerR = if (controller2.rightTrigger > 25 || controller2.leftTrigger > 25) {
+            controller2.rightTrigger - (controller2.leftTrigger * 0.5)
+        } else powerL
+
+        Devices.motor4.speed = powerL
+        Devices.motor5.speed = -powerL
+        Devices.motor6.speed = powerR
+        Devices.motor7.speed = -powerR
     }
 }
