3fd64b9264178060d07e5b5c03c83e1e73d8ecdd
Thomas Ricci <git@thomasricci.dev>
2023-01-27 17:55:49 -0500

    ux!!! i guess its ux

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SmallbotProductionOdo.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import org.firstinspires.ftc.teamcode.features.FourMotorArm;
+import org.firstinspires.ftc.teamcode.features.Hand;
+import org.firstinspires.ftc.teamcode.features.MecanumDrivetrain;
+import org.firstinspires.ftc.teamcode.internals.registration.OperationMode;
+import org.firstinspires.ftc.teamcode.internals.registration.TeleOperation;
+
+public class SmallbotProductionOdo extends OperationMode implements TeleOperation {
+
+    @Override
+    public void construct() {
+        registerFeature(new MecanumDrivetrain(true, true));
+        registerFeature(new FourMotorArm());
+        registerFeature(new Hand());
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
