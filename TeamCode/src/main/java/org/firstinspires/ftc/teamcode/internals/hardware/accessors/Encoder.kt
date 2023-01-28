c41d6db3d85a8b017597d5423ddcbe079163e126
Thomas Ricci <git@thomasricci.dev>
2023-01-28 11:34:54 -0500

    refactor encoders to use new system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internals/hardware/accessors/Encoder.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internals/hardware/accessors/Encoder.kt
@@ -8,19 +8,19 @@ import org.firstinspires.ftc.teamcode.internals.hardware.data.EncoderInput
 /**
  * An encoder tracks the number of rotations of a hex rod and can be used to measure distance.
  */
-class Encoder(override var name: String): DeviceAccessor("encoder$name") {
+class Encoder(override var name: String): DeviceAccessor(name) {
 
     /**
      * The jlooping request managing the underlying hardware.
      */
-    val request: ScriptParameters.Request = HardwareGetter.makeEncoderRequest("$name")
+    val request: ScriptParameters.Request = HardwareGetter.makeEncoderRequest(name)
 
     /**
      * The underlying encoder managed as a DC motor being managed by the jlooping request.
      */
     val encoder: DcMotor
         get() {
-            return HardwareGetter.getEncoderFromRequest("encoder$name")
+            return HardwareGetter.getEncoderFromRequest(name)
         }
 
     /**
@@ -28,14 +28,14 @@ class Encoder(override var name: String): DeviceAccessor("encoder$name") {
      */
     val position: Int
         get() {
-            return HardwareGetter.issueEncoderRequest("encoder$name", EncoderInput.GET)
+            return HardwareGetter.issueEncoderRequest(name, EncoderInput.GET)
         }
 
     /**
      * Resets the encoder to 0.
      */
     fun reset() {
-        HardwareGetter.issueEncoderRequest("encoder$name", EncoderInput.SET)
+        HardwareGetter.issueEncoderRequest(name, EncoderInput.SET)
     }
 
 }
